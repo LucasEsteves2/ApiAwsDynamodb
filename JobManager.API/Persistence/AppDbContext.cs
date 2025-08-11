cbe2f8e51dfb44337b524f5e31ab8974e44c9949
LucasEsteves2 <t2m.lrodrigues@terceiros.mag.com.br>
2025-08-11 12:00:00 -0300

    add AppDbContext

--- JobManager.API/Persistence/AppDbContext.cs
+++ JobManager.API/Persistence/AppDbContext.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using JobManager.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobManager.API.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Job>(e =>
+            {
+                e.HasKey(j => j.Id);
+
+                e.HasMany(j => j.Applications)
+                    .WithOne(a => a.Job)
+                    .HasForeignKey(a => a.JobId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<JobApplication>(e => e.HasKey(j => j.Id));
+        }
+    }
+}
+
+
